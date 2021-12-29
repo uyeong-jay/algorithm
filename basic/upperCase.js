d0bbcfe94558ef6c501f75cc6d00e9367c421761
william-jacob <wjacob2103@gmail.com>
2021-12-29 10:57:55 +0900

    [ADD && UPDATE] 대문자 갯수 세기

---
+++ basic/upperCase.js
@@ -0,0 +1,19 @@
+//대문자 갯수 세기
+
+const string = "KimchiIsKoreanFood";
+
+function solution(str) {
+  let answer = 0;
+  for (let i of str) { //배열 요소 루프 생성
+    let num = i.charCodeAt() // 아스키코드 넘버로 저장( or toUpperCase())
+    if (65 <= num && num <= 90) { //대문자만
+      // A~Z: 범위 나누기
+      // A~Z(대문자): 65 ~ 90
+      // a~b(소문자): 97 ~ 122
+      answer++;
+    }
+  }
+  return answer;
+}
+
+console.log(solution(string)); //4
