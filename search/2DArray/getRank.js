1e22c23581a98593aac6f576a192926f1b9c74b6
william-jacob <wjacob2103@gmail.com>
2022-03-21 16:23:39 +0900

    [ADD] 순위 매기기

---
+++ search/2DArray/getRank.js
@@ -0,0 +1,24 @@
+//순위 매기기
+
+const array = [89, 89, 92, 100, 76];
+
+const solution = (arr) => {
+  let n = arr.length;
+  let answer = Array(n).fill(1); //순위 배열 만들기
+
+  //이중반복문 사용
+  arr.forEach((v1, i) => {
+    arr.forEach((v2, j) => {
+      if (arr[i] < arr[j]) { //순위 비교하기
+        answer[i]++;
+      }
+    });
+  });
+
+  return answer;
+};
+
+console.log(solution(array)); //[ 3, 3, 2, 1, 5 ]
+
+//tip
+//이중반복문: 배열 요소중 하나를 기준으로 나머지 요소들을 탐색해야할때 사용
